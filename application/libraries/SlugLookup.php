170dd0527466b977a47be78fe11b2fda8b7f6166
yitznewton <yitzchak.schaffer@gmx.com>
2010-01-06 14:33:22 -0500

    more on slugs

--- application/libraries/SlugLookup.php
+++ application/libraries/SlugLookup.php
@@ -2,33 +2,55 @@
 
 class SlugLookup
 {
+  protected static $buildLock = null;
+  protected $collection;
   protected $pdo;
 
   function __construct( Collection $collection )
   {
-    $filepath = APPPATH . 'data/';
-    $filename = $filepath . $collection->getName()
+    $this->collection = $collection;
+
+    $db_filepath = APPPATH . 'data/';
+    $db_filename = $db_filepath . $collection->getName()
                 . '_slugs.db';
 
-    if (! file_exists( $filename ) && ! is_writable( $filepath )) {
-      throw new Exception("Could not write to data path $filepath");
+    if (! file_exists( $db_filename ) && ! is_writable( $db_filepath )) {
+      throw new Exception("Could not write to data path $db_filepath");
     }
 
-    $this->pdo = new PDO( 'sqlite:' . $filename );
+    $this->pdo = new PDO( 'sqlite:' . $db_filename );
     
+    ($elements = $collection->getConfig('slug_metadata_elements'))
+      or $elements = array( 'Title' );
+
+    $elements_string = serialize( $elements );
+
+    $query = 'SELECT value FROM metadata WHERE key="slug_metadata_elements"';
+    $stmt = $this->pdo->query( $query );
+
+    if (! $stmt || $stmt->fetchColumn() != $elements_string) {
+      // changed metadata settings since last build; backup and rebuild
+      copy( $db_filename, $db_filename . '.bak' );
+      $this->buildFull();
+
+      //return;
+    }
+    var_dump($elements_string);exit;
+
     $query  = 'SELECT value FROM metadata WHERE key="build_date"';
     $stmt = $this->pdo->query( $query );
 
     if (! $stmt) {
       // database corrupt or absent; full build
-      $this->build( true );
+      $this->buildFull;
     }
     
     $build_date = $stmt->fetchColumn();
 
     if ($collection->getBuildCfg()->getBuildDate() > $build_date) {
       // collection was built since last slug build; do incremental build
-      $this->build();
+      $this->buildIncremental();
+      // return;
     }
   }
 
@@ -43,18 +65,45 @@ class SlugLookup
     // not set - compose based on slug metadata config
   }
 
-  protected function build( $full_build = false )
+  protected function buildFull()
   {
-    if ($full_build === true) {
-      $query1 = 'DROP TABLE IF EXISTS metadata';
-      $query2 = 'DROP TABLE IF EXISTS slugs';
-      $query3 = 'CREATE TABLE metadata (key VARCHAR(25), value VARCHAR)';
-      $query4 = 'CREATE TABLE slugs (key VARCHAR(25), value VARCHAR)';
+    if ( isset( self::$buildLock ) && (self::$buildLock > (time() - 8)) ) {
+      // recent build in progress; wait and see if it succeeds
+      sleep( 7 );
 
-      $this->pdo->exec( $query1 );
-      $this->pdo->exec( $query2 );
-      $this->pdo->exec( $query3 );
-      $this->pdo->exec( $query4 );
+      if (empty( self::$buildLock )) {
+        // the other build succeeded
+        return false;
+      }
     }
+
+    self::$buildLock = time();
+
+    ($elements = $this->collection->getConfig('slug_metadata_elements'))
+      or $elements = array( 'Title' );
+
+    $elements_string = serialize( $elements );
+    $build_date = $this->collection->getBuildCfg()->getBuildDate();
+
+    if (! preg_match( '/^\d+$/', $build_date )) {
+      throw new Exception('Invalid build date');
+    }
+
+    $query = <<<EOF
+      DROP TABLE IF EXISTS metadata; DROP TABLE IF EXISTS slugs;
+      CREATE TABLE metadata (key VARCHAR, value VARCHAR);
+      CREATE TABLE slugs (key VARCHAR(25), base_slug VARCHAR, slug VARCHAR);
+      INSERT INTO metadata VALUES ('build_date', '$build_date');
+EOF;
+
+    $this->pdo->exec( $query );
+
+    $query = 'INSERT INTO metadata VALUES ("elements_string", ?)';
+    $stmt = $this->pdo->prepare( $query );
+    $stmt->execute( array( $elements_string ) );
+
+    // go through all [nodes?] and formulate & store slugs for them
+
+    self::$buildLock = null;
   }
 }
