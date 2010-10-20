401d5510756ec1683c97d03e911f09aeed95c60f
yitznewton <yitzchak.schaffer@gmx.com>
2010-10-20 17:22:15 -0400

    modify Greenstone scripts

--- greenstone/raw_text_extract.php
+++ greenstone/raw_text_extract.php
@@ -15,11 +15,11 @@
  * @version 0.2.0-b4
  * @package greenstone
  *
- * This script extracts raw text from Greenstone documents for processing by
+ * This script processes metadata from the Greenstone build process for
  * EmeraldView's search snippet code
  */
 if ( $argc != 3 ) {
-  echo 'Usage: raw_text_extract.php build_directory index_sub_directory' . "\n";
+  echo 'Usage: raw_text_extract.php build_directory index_sub_directory' . chr(10);
   exit(1);
 }
 
@@ -30,7 +30,7 @@ $raw_text_sub_dir = $build_dir . '/raw-text/' . $index_sub_dir;
 
 if ( ! is_dir( $raw_text_dir ) ) {
   $mkdir = @mkdir( $raw_text_dir );
-
+  
   if ( ! $mkdir ) {
     throw new Exception( 'Could not create raw text directory' );
   }
@@ -38,7 +38,7 @@ if ( ! is_dir( $raw_text_dir ) ) {
 
 if ( ! is_dir( $raw_text_sub_dir ) ) {
   $mkdir = @mkdir( $raw_text_sub_dir );
-
+  
   if ( ! $mkdir ) {
     throw new Exception( 'Could not create raw text subdirectory' );
   }
@@ -52,10 +52,10 @@ while ( $line = fgets( STDIN ) ) {
       // already accumulated one
       process_xml_blob( $xml_blob, $raw_text_sub_dir );
     }
-
-    $xml_blob = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+    
+    $xml_blob = '<?xml version="1.0" encoding="UTF-8"?>' . chr(10);
   }
-
+  
   $xml_blob .= $line;
 }
 
@@ -65,7 +65,7 @@ process_xml_blob( $xml_blob, $raw_text_sub_dir );  // the last document
 function process_xml_blob( $blob, $directory )
 {
   $first_chunk = substr( $blob, 0, 400 );
-
+  
   if (
     strpos( $first_chunk, 'gs2:mode="add"' ) === false
     && strpos( $first_chunk, 'gs2:mode="update"' ) === false
@@ -73,29 +73,29 @@ function process_xml_blob( $blob, $directory )
     // looks like a delete
     return false;
   }
-
+  
   $doc_dom = DOMDocument::loadXML( $blob );
   $xpath = new DOMXPath( $doc_dom );
   $sections = $xpath->query( '/Doc/Sec[@gs2:mode!="delete"]' );
-
+  
   foreach ( $sections as $section ) {
-    process_section( $section, $directory );
+    extract_section( $section, $directory );
   }
 }
 
-function process_section( DOMNode $dom_node, $directory )
+function extract_section( DOMNode $dom_node, $directory )
 {
   foreach ( $dom_node->childNodes as $child ) {
     if ( $child->nodeName == 'TX' ) {
       $sec_id = $dom_node->attributes
                 ->getNamedItemNS( 'http://www.greenstone.org/gs2', 'docOID' )
                 ->value;
-
+                
       $text = $child->textContent;
-
+      
       $fh = fopen( $directory . '/' . $sec_id . '.txt', 'wb' );
       fwrite( $fh, $text );
-
+      
       return;
     }
   }
--- greenstone/solr_extract.php
+++ greenstone/solr_extract.php
@@ -1,18 +1,39 @@
 <?php
-
+/**
+ * EmeraldView
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://yitznewton.org/emeraldview/index.php?title=License
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to yitznewton@hotmail.com so we can send you a copy immediately.
+ *
+ * @version 0.2.0-b4
+ * @package greenstone
+ *
+ * This script processes metadata from the Greenstone build process for
+ * ingest into Solr
+ */
 if ( $argc != 3 ) {
-  echo 'Usage: solr_extract.php build_directory index_sub_directory' . "\n";
+  echo 'Usage: solr_extract.php build_directory index_sub_directory' . chr(10);
   exit(1);
 }
 
-$build_dir     = $argv[1];
-$output_file   = $build_dir . '/solr.xml';
+$index_level = $argv[2];
 
-$output_fh = @fopen( $output_file, 'ab' );
+$build_dir   = $argv[1];
+$output_file = $build_dir . '/solr-' . $index_level . '.xml';
+$output_fh   = @fopen( $output_file, 'wb' );
 
 if ( ! $output_fh ) {
   throw new Exception( 'Could not open output for appending' );
 }
+
+fwrite( $output_fh, '<add>' . chr(10) );
 
 $xml_in_blob = '';
 
@@ -20,21 +41,21 @@ while ( $line = fgets( STDIN ) ) {
   if ( substr( $line, 0, 4 ) == '<Doc' ) {
     if ( $xml_in_blob ) {
       // already accumulated one
-      process_xml_doc( $xml_in_blob, $output_fh );
+      process_xml_doc( $xml_in_blob, $output_fh, $index_level );
     }
     
-    $xml_in_blob = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+    $xml_in_blob = '<?xml version="1.0" encoding="UTF-8"?>' . chr(10);
   }
   
   $xml_in_blob .= $line;
 }
 
-process_xml_doc( $xml_in_blob, $output_fh );  // the last document
+process_xml_doc( $xml_in_blob, $output_fh, $index_level );  // the last document
 
-fwrite( $output_fh, '</add>' );
+fwrite( $output_fh, '</add>' . chr(10) );
 
 
-function process_xml_doc( $blob, $fh )
+function process_xml_doc( $blob, $fh, $index_level )
 {
   $first_chunk = substr( $blob, 0, 400 );
   
@@ -48,7 +69,19 @@ function process_xml_doc( $blob, $fh )
 
   $doc_dom = DOMDocument::loadXML( $blob );
   $xpath = new DOMXPath( $doc_dom );
-  $sections = $xpath->query( '/Doc/Sec[@gs2:mode!="delete"]' );
+  
+  if ( $index_level == 'didx' ) {
+    $querystring = '/Doc[@gs2:mode!="delete"]';
+  }
+  elseif ( $index_level == 'sidx' ) {
+    $querystring = '/Doc/Sec[@gs2:mode!="delete"]';
+  }
+  else {
+    echo 'Unrecognized index level' . chr(10);
+    exit(1);
+  }
+  
+  $sections = $xpath->query( $querystring );
   
   foreach ( $sections as $section ) {
     fwrite( $fh, '<doc>' . chr(10) );
